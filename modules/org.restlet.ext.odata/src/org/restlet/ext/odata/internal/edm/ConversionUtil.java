9777c9e1cdb36986224213efb3280e3d74660811
Akshay <akshayharale007@gmail.com>
2015-01-14 13:31:15 +0530

    Encoding url to resolve the update and delete issue for entity which have special character in it's primary key.

---
+++ modules/org.restlet.ext.odata/src/org/restlet/ext/odata/internal/edm/ConversionUtil.java
@@ -0,0 +1,102 @@
+package org.restlet.ext.odata.internal.edm;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+
+/**
+ * 
+ * This class uses URLEncoder to Encode the String and URLDecoder to decode the String.
+ *<ol>
+ *  <li>The alphanumeric characters "a" through "z", "A" through "Z" and "0" through "9" remain the same.</li>
+ *  <li>The special characters ".", "-", "*", and "_" remain the same.</li>
+ *  <li>The space character " " is converted into a plus sign "+".</li>
+ *  <li>All other characters are unsafe and are first converted into one or more bytes using some encoding scheme.</li>
+ *</ol>
+ *Then each byte is represented by the 3-character string "%xy", where xy is the two-digit hexadecimal representation of the byte.<br>
+ *The recommended encoding scheme to use is UTF-8. However, for compatibility reasons, if an encoding is not specified, then the default encoding of the platform is used.
+ *
+ */
+public class ConversionUtil {
+
+  /** The Constant log. */
+  private static final Logger log = Logger.getLogger(ConversionUtil.class.getName());
+
+   /** The Constant encodingScheme. */
+  private static final String encodingOrDecodingScheme = "UTF-8";
+
+  /** The Constant UNSUPPORTED_CHARACTER. */
+  private static final Map<String, String> UNSUPPORTED_CHARACTER = new HashMap<String, String>();
+  static {
+    UNSUPPORTED_CHARACTER.put("+", "%20");
+    UNSUPPORTED_CHARACTER.put("%2F", "/");
+    UNSUPPORTED_CHARACTER.put("%27", "'");
+    UNSUPPORTED_CHARACTER.put("%3D", "=");
+    UNSUPPORTED_CHARACTER.put("%2C", ",");
+    UNSUPPORTED_CHARACTER.put("%28", "(");
+    UNSUPPORTED_CHARACTER.put("%29", ")");
+   }
+
+  /**
+   * Instantiates a new conversion util.
+   */
+  private ConversionUtil() {
+
+  }
+
+  /**
+   * Encode string.
+   *
+   * @param stringToEncode the string to encode
+   * @return the string
+   * @throws UnsupportedEncodingException the unsupported encoding exception
+   */
+  public static String encodeString(String stringToEncode) {
+    String encodedString =  null;
+    if (stringToEncode != null) {
+      try {
+        encodedString = ConversionUtil.removeUnsuportedCharacter(URLEncoder.encode(stringToEncode, encodingOrDecodingScheme));
+      } catch (UnsupportedEncodingException e) {
+        log.info(e.getMessage());
+      }
+    }
+    return encodedString;
+  }
+
+  /**
+   * Decode string.
+   *
+   * @param stringToDecode the string to decode
+   * @return the string
+   */
+  public static String decodeString(String stringToDecode) {
+    String decodedString = null;
+    if (stringToDecode != null) {
+      try {
+        decodedString = URLDecoder.decode(stringToDecode, encodingOrDecodingScheme);
+      } catch (UnsupportedEncodingException e) {
+        log.warning("ConversionUtil failed to decode a string:"+stringToDecode+"\nthe error is"+e.getMessage());
+      }
+    }
+    return decodedString;
+  }
+
+  /**
+   * Removes the unsuported character.
+   *
+   * @param encodedString the encoded string
+   * @return the string
+   */
+  private static String removeUnsuportedCharacter(String encodedString) {
+    for (Map.Entry<String, String> e : UNSUPPORTED_CHARACTER.entrySet()) {
+      encodedString = encodedString.replace(e.getKey(), e.getValue());
+    }
+    return encodedString;
+
+  }
+  
+}
